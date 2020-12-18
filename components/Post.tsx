c0e049403456063e007bf0b17a694ec6c558ef26
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-18 15:30:00 +0700

    ✏️ Update content for proper deployment

--- components/Post.tsx
+++ components/Post.tsx
@@ -3,17 +3,21 @@ import Head from "next/head";
 type Props = React.PropsWithChildren<{
   title: string;
   description: string;
+  createdAt: Date;
 }>;
 
-const Post = ({ title, description, children }: Props) => (
+const Post = ({ title, description, createdAt, children }: Props) => (
   <>
     <Head>
-      <title>{title}</title>
+      <title>{title} · Minh-Phuc Tran</title>
       <meta name="description" content={description} />
     </Head>
     <article>
       <header>
         <h1>{title}</h1>
+        <sup>
+          <em>{createdAt.toLocaleDateString()}</em>
+        </sup>
       </header>
       <main>{children}</main>
     </article>
