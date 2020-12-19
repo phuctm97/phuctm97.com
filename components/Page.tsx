d9a2c707658317fd7489c7ca4305e916a17c788e
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-19 11:19:31 +0700

    Use summary_large_image Twitter card

--- components/Page.tsx
+++ components/Page.tsx
@@ -12,8 +12,10 @@ const Page = ({ title, description, children }: Props) => (
       <meta name="description" content={description} />
       <meta name="og:title" content={title} />
       <meta name="og:description" content={description} />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="@phuctm97" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content="/images/twitter-card.png" />
+      <meta name="twitter:image:alt" content="Minh-Phuc Tran @phuctm97" />
+      <meta name="twitter:creator:id" content="1163045996755951619" />
     </Head>
     {children}
   </>
