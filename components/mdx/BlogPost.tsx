2911d82676451985e286d8a3448e3572c9c67531
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-19 09:49:52 +0700

    🗑 Remove unnessary mdx components

--- components/mdx/BlogPost.tsx
+++ components/mdx/BlogPost.tsx
@@ -2,8 +2,8 @@ import Post from "~components/Post";
 
 type Metadata = Omit<React.ComponentProps<typeof Post>, "children">;
 
-const post = (metadata: Metadata): React.FC => ({ children }) => (
+const BlogPost = (metadata: Metadata): React.FC => ({ children }) => (
   <Post {...metadata}>{children}</Post>
 );
 
-export default post;
+export default BlogPost;
