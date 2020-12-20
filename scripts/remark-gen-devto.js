f267a0aa162d6b1d44872dba820f0c6f951b3a2a
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-20 16:08:09 +0700

    Fix remark gen DEV.to

--- scripts/remark-gen-devto.js
+++ scripts/remark-gen-devto.js
@@ -6,40 +6,55 @@ const toMarkdown = require("mdast-util-to-markdown");
 
 const CanonicalURL = "https://phuctm97";
 const DEVToID = "dev.to id";
+const DEVToURL = "dev.to url";
 const DEVToDir = path.join(process.cwd(), "dev.to");
 const DEVToAPIKey = process.env.DEVTO_API_KEY ?? "";
 
-const readDEVToID = (filename) => {
+const readDEVToIdentifiers = (filename) => {
   const { data } = matter(fs.readFileSync(filename, "utf-8"));
 
   const id = data[DEVToID];
-  if (!id) {
+  const url = data[DEVToURL];
+  if (!id || !url) {
     const relName = path.relative(DEVToDir, devToFilename);
-    return file.fail(`Couldn't find attribute '${DEVToID}' in '${relName}'`);
+    return file.fail(
+      `'${relName}' must have frontmatter '${DEVToID}' and '${DEVToURL}'.`
+    );
   }
 
-  return id;
+  return [id, url];
 };
 
-const createDEVToID = async (title, filename) => {
-  console.log("sending x");
-  // const res = await axios.post(
-  //   "https://dev.to/api/articles",
-  //   {
-  //     article: {
-  //       title,
-  //       body_markdown: [
-  //         `#${title}`,
-  //         `File: ${filename}`,
-  //         "Working in progress",
-  //       ].join("\n"),
-  //       published: false,
-  //     },
-  //   },
-  //   { headers: { "api-key": DEVToAPIKey } }
-  // );
+const beautifyAxiosErrorMessage = (err) => {
+  let msg = err.message;
+  if (err.response) msg += `\n${JSON.stringify(err.response.data, null, 2)}`;
 
-  // return res.id;
+  return msg;
+};
+
+const createDEVToArticle = async (title, filename) => {
+  console.log("Creating article");
+
+  try {
+    const res = await axios.post(
+      "https://dev.to/api/articles",
+      {
+        article: {
+          title,
+          body_markdown: [
+            `#${title}`,
+            `File: ${filename}`,
+            "Working in progress",
+          ].join("\n"),
+          published: false,
+        },
+      },
+      { headers: { "api-key": DEVToAPIKey } }
+    );
+    return [res.id, res.url];
+  } catch (err) {
+    throw new Error(beautifyAxiosErrorMessage(err));
+  }
 };
 
 module.exports = () => async (_, file) => {
@@ -50,13 +65,14 @@ module.exports = () => async (_, file) => {
   const { subpage, slug, path: urlPath } = page;
 
   const devToFilename = path.join(DEVToDir, subpage, slug + ".md");
-  const devToID = fs.existsSync(devToFilename)
-    ? readDEVToID(devToFilename)
-    : await createDEVToID(title, devToFilename);
-  if (!devToID) return;
+  const [devToID, devToURL] = fs.existsSync(devToFilename)
+    ? readDEVToIdentifiers(devToFilename)
+    : await createDEVToArticle(title, devToFilename);
+  if (!devToID || !devToURL) return;
 
   const devToFrontmatter = {
-    "dev.to id": devToID,
+    [DEVToID]: devToID,
+    [DEVToURL]: devToURL,
     title,
     description,
     canonical_url: `${CanonicalURL}/${urlPath}`,
