d71d4724d819c913500cf97859ba352c7e75c3f8
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-20 19:55:43 +0700

    Remove gen DEV.to

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const remarkExtractFrontmatter = require("remark-frontmatter");
 const remarkParseFrontmatter = require("./scripts/remark-parse-frontmatter");
 const remarkTransformPage = require("./scripts/remark-transform-page");
-const remarkGenDEVTo = require("./scripts/remark-gen-devto");
 const remarkA11yEmoji = require("@fec/remark-a11y-emoji");
 
 const withMDX = require("@next/mdx")({
@@ -10,7 +9,6 @@ const withMDX = require("@next/mdx")({
       remarkExtractFrontmatter,
       remarkParseFrontmatter,
       remarkTransformPage,
-      remarkGenDEVTo,
       remarkA11yEmoji,
     ],
     rehypePlugins: [],
