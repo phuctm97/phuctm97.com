d11a7028926c5a5a4c72f3510bc39258766415b3
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-20 11:29:14 +0700

    Add script to find non-distributed file

---
+++ scripts/get-nondistributed-article.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const fs = require("fs");
+const yaml = require("yaml");
+const vfile = require("to-vfile");
+const find = require("unist-util-find");
+const { createCompiler } = require("@mdx-js/mdx");
+const remarkFrontmatter = require("remark-frontmatter");
+
+// Find all MDX filenames
+const dir = path.join(process.cwd(), "pages", "blog");
+const filenames = fs
+  .readdirSync(dir)
+  .filter((name) => /\.mdx$/.test(name))
+  .map((name) => path.join(dir, name));
+
+// Create custom MDX loader to extract frontmatter.
+const extractFrontmatter = () => (tree, file) => {
+  const node = find(tree, { type: "yaml" });
+  if (!node) return;
+
+  file.data.frontmatter = yaml.parse(node.value);
+};
+const mdxc = createCompiler({
+  remarkPlugins: [remarkFrontmatter, extractFrontmatter],
+});
+
+// Find non-distributed file.
+let result;
+for (let filename of filenames) {
+  const file = vfile.readSync(filename, "utf8");
+
+  mdxc.process(file, (err, file) => {
+    if (err) throw err;
+    if (!file.data.frontmatter) return;
+    if (file.data.frontmatter["dev.to id"]) return;
+
+    result = filename;
+  });
+
+  if (result) break;
+}
+
+console.log("Non distributed file:", result);
