88079f732413617f254fabf83dd985adfe76392d
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-21 18:01:51 +0700

    ⏳ Delay 5 seconds between DEV.to API calls

--- bin/devto.js
+++ bin/devto.js
@@ -6,18 +6,29 @@ const agent = axios.create({
   headers: { "api-key": process.env.DEVTO_API_KEY },
 });
 
+const delay = (duration) =>
+  new Promise((resolve) => setTimeout(resolve, duration));
+
 const createArticle = async ({ frontmatter, content }) => {
-  const { data } = await agent.post("/articles", {
-    article: { ...frontmatter, body_markdown: content },
-  });
-  return data;
+  try {
+    const { data } = await agent.post("/articles", {
+      article: { ...frontmatter, body_markdown: content },
+    });
+    return data;
+  } finally {
+    await delay(5000);
+  }
 };
 
 const updateArticle = async (id, { frontmatter, content }) => {
-  const { data } = await agent.put(`/articles/${id}`, {
-    article: { ...frontmatter, body_markdown: content },
-  });
-  return data;
+  try {
+    const { data } = await agent.put(`/articles/${id}`, {
+      article: { ...frontmatter, body_markdown: content },
+    });
+    return data;
+  } finally {
+    await delay(5000);
+  }
 };
 
 module.exports = {
