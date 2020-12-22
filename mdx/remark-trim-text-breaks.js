8ac354354cf3ccbd559d27c138347a842305374b
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-22 17:16:27 +0700

    👾 Fix invalid trim newlines (#24)

--- mdx/remark-trim-text-breaks.js
+++
@@ -1,8 +0,0 @@
-const visit = require("unist-util-visit");
-
-module.exports = () => (tree) => {
-  visit(tree, "text", (node, index, parent) => {
-    if (node.value === "\n") parent.children.splice(index, 1);
-    else node.value = node.value.replace(/\n/g, "");
-  });
-};
