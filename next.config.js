d42d4118c2c14274c7387cd0f7dbc49202232f39
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-22 21:00:41 +0700

    Reorder next mdx plugins

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ const withMDX = require("@next/mdx")({
   options: {
     remarkPlugins: [
       require("remark-frontmatter"),
-      require("./scripts/mdx/remark-unwrap-texts"),
       require("./scripts/mdx/remark-parse-frontmatter"),
       require("./scripts/mdx/remark-page-metadata"),
+      require("./scripts/mdx/remark-unwrap-texts"),
       require("./scripts/mdx/remark-page-layout"),
     ],
     rehypePlugins: [require("rehype-accessible-emojis").rehypeAccessibleEmojis],
