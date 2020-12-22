f0cce60b547afb589fb01e0ba064ae43ff7e7edc
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-22 16:58:06 +0700

    Fix DEV.to API

--- bin/devto-api.js
+++ bin/devto-api.js
@@ -3,33 +3,30 @@ const { delay } = require("./utils");
 
 const agent = axios.create({
   baseURL: "https://dev.to/api",
-  timeout: 5000,
   headers: { "api-key": process.env.DEVTO_API_KEY },
+  timeout: 10000,
 });
 
 const throttleDuration = 5000;
 
-const throttle = async (fn) => {
-  try {
-    return await fn();
-  } finally {
-    await delay(throttleDuration);
-  }
-};
+const throttle = (fn) => (...args) =>
+  Promise.all([fn(...args), delay(throttleDuration)]);
 
-const createArticle = throttle(async ({ frontmatter, content }) => {
-  const { data } = await agent.post("/articles", {
-    article: { ...frontmatter, body_markdown: content },
-  });
-  return data;
-});
+const createArticle = throttle(({ frontmatter, content }) =>
+  agent
+    .post("/articles", {
+      article: { ...frontmatter, body_markdown: content },
+    })
+    .then(({ data }) => data)
+);
 
-const updateArticle = throttle(async (id, { frontmatter, content }) => {
-  const { data } = await agent.put(`/articles/${id}`, {
-    article: { ...frontmatter, body_markdown: content },
-  });
-  return data;
-});
+const updateArticle = throttle((id, { frontmatter, content }) =>
+  agent
+    .put(`/articles/${id}`, {
+      article: { ...frontmatter, body_markdown: content },
+    })
+    .then(({ data }) => data)
+);
 
 module.exports = {
   createArticle,
