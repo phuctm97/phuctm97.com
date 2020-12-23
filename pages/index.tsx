c14ed910441dd1e104f690fe86bbe403ec583a50
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-23 12:03:15 +0700

    ✨ Update landing and frontmatter for distributing to DEV.to (#31)

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,14 +30,25 @@ const IndexPage = () => {
         <h1>Hi, I'm Minh-Phuc Tran</h1>
         <p>
           I'm a Software Engineer. I share my learnings and opinions about
-          software development here.Hope it helps.
+          software development here. Hope it helps.
         </p>
         <p>
           <em>
-            I'm rebuilding my website, meanwhile you can checkout my articles at{" "}
-            <a href="https://blog.phuctm97.com">blog.phuctm97.com</a>,{" "}
-            <a href="https://phuctm97.medium.com">Medium</a>, and{" "}
-            <a href="https://dev.to/phuctm97">DEV.to</a>.
+            My website is currently in just HTML, though it's good enough to
+            read, my articles are automatically distributed to{" "}
+            <a href="https://dev.to/phuctm97">
+              my DEV.to, feel free to go there
+            </a>{" "}
+            if you prefer more colorful format. There are also about 50+ other
+            blog posts on{" "}
+            <a href="https://blog.phuctm97.com">blog.phuctm97.com</a> and{" "}
+            <a href="https://phuctm97.medium.com">Medium</a>.
+          </em>
+        </p>
+        <p>
+          <em>
+            Besides, <a href="https://twitter.com/phuctm97">DM me on Twitter</a>{" "}
+            if you have any question or need help.
           </em>
         </p>
       </header>
