8077bdbbb0739b6f473dbfeb720bc31ccc1cb099
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-25 15:31:41 +0700

    Apply base styles, add sass support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { DefaultSeo } from "next-seo";
-import seoProps from "next-seo.config";
+import seo from "next-seo.config";
 import { MDXProvider } from "@mdx-js/react";
-import * as MDXComponents from "~components/mdx/Components";
+import * as MDXComponents from "~components/mdx/base";
 import "~styles/app.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
@@ -11,7 +11,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head>
-    <DefaultSeo {...seoProps} />
+    <DefaultSeo {...seo} />
     <MDXProvider components={MDXComponents}>
       <Component {...pageProps} />
     </MDXProvider>
