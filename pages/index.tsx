381c6bc270445ecb68c56b277243a88ece00dc5b
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2020-12-28 20:19:43 +0700

    Fix homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Header from "~components/header";
-import styles from "./index.module.scss";
+import Main from "~components/main";
 
 const IndexPage = () => {
   const posts = [
@@ -45,57 +45,73 @@ const IndexPage = () => {
   return (
     <>
       <Header />
-      <main className={styles.page}>
-        <div className={styles.container}>
-          <h1>Hey, I'm Minh-Phuc Tran.</h1>
-          <p>
-            I'm a Software Engineer. Growing as a developer can be lonely and
-            challenging sometimes, I document my ups and downs here to hopefully
-            motivate you. If you're looking for help or just a friend,{" "}
-            <a href="https://twitter.com/phuctm97">
-              feel free to DM me on Twitter
-            </a>
-            , I'll reply as soon as possible.
+      <Main>
+        <h1 className="text-black font-bold text-4xl mb-8">
+          Hey, I'm Minh-Phuc Tran.
+        </h1>
+        <p className="text-gray-900 mb-8">
+          I'm a Software Engineer. Growing as a developer can be lonely and
+          challenging sometimes, I document my ups and downs here to hopefully
+          motivate you. If you're looking for help or just a friend,{" "}
+          <a href="https://twitter.com/phuctm97">
+            feel free to DM me on Twitter
+          </a>
+          , I'll reply as soon as possible.
+        </p>
+        <section className="mb-8">
+          <h2 className="text-black font-bold text-2xl mb-4">Newsletter</h2>
+          <p className="text-gray-900 mb-4">
+            In my newsletter, I share early and behind-the-scene look into what
+            I’m working on and learning about software development. Most
+            importantly, it's a way for you and me to establish a closer
+            relationship and hopefully can help each other in the future.
           </p>
-          <section>
-            <h2>Newsletter</h2>
-            <p>
-              In my newsletter, I share early and behind-the-scene look into
-              what I’m working on and learning about software development. Most
-              importantly, it's a way for you and me to establish a closer
-              relationship and hopefully can help each other in the future.
-            </p>
-            <form
-              action="https://buttondown.email/api/emails/embed-subscribe/phuctm97"
-              method="post"
-              target="popupwindow"
-              onSubmit={onSubscribeNewsletter}
-            >
-              <label htmlFor="bd-email">Enter your email: </label>
-              <input type="email" name="email" id="bd-email" />
-              <input type="hidden" value="1" name="embed" />
-              <input type="submit" value="Subscribe" />
-            </form>
-            <sup>
-              <em>(No spam, unsubscribe anytime)</em>
-            </sup>
-          </section>
-          <section>
-            <h2>Blog</h2>
-            <p>
-              My free-flowing thoughts and ideas: 100% authentic, good for
-              inspiration, not always true.
-            </p>
-            <ul>
-              {posts.map(({ link, text }) => (
-                <li key={link}>
-                  <Link href={link}>{text}</Link>
-                </li>
-              ))}
-            </ul>
-          </section>
-        </div>
-      </main>
+          <form
+            action="https://buttondown.email/api/emails/embed-subscribe/phuctm97"
+            method="post"
+            target="popupwindow"
+            onSubmit={onSubscribeNewsletter}
+          >
+            <label htmlFor="bd-email" className="text-gray-900">
+              Enter your email:{" "}
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="bd-email"
+              className="border border-gray-500 rounded-sm p-1 mr-2"
+            />
+            <input type="hidden" value="1" name="embed" />
+            <input
+              type="submit"
+              value="Subscribe"
+              className="bg-gray-100 py-1 px-3 rounded-sm"
+            />
+          </form>
+          <p className="text-sm ml-28 text-gray-900">
+            <em>(No spam, unsubscribe anytime)</em>
+          </p>
+        </section>
+        <section>
+          <h2 className="text-black font-bold text-2xl mb-4">Blog</h2>
+          <p className="mb-4">
+            My free-flowing thoughts and ideas: 100% authentic, good for
+            inspiration, not always true.
+          </p>
+          <ul>
+            {posts.map(({ link, text }) => (
+              <li key={link} className="mt-2 ">
+                ·{"  "}
+                <Link href={link}>
+                  <a className="text-blue-600 font-medium underline hover:text-blue-700">
+                    {text}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </Main>
     </>
   );
 };
