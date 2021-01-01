4af4787a98d05b1df8665176bba34f15c492cb84
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-01 17:38:17 +0700

    Add ul style

--- pages/styles.tsx
+++ pages/styles.tsx
@@ -96,6 +96,25 @@ const StylesPage = () => (
       </li>
       <li>Fourth item</li>
     </ol>
+
+    <ul>
+      <li>First item</li>
+      <li>Second item</li>
+      <li>Third item</li>
+      <li>Fourth item</li>
+    </ul>
+    <ul>
+      <li>First item</li>
+      <li>Second item</li>
+      <li>
+        Third item
+        <ul>
+          <li>Indented item</li>
+          <li>Indented item</li>
+        </ul>
+      </li>
+      <li>Fourth item</li>
+    </ul>
   </main>
 );
 
