aaffc8cdb0dead02caa86aa2732196f65e5dbd83
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-01 17:07:47 +0700

    Add bold and em styles

--- pages/styles.tsx
+++ pages/styles.tsx
@@ -1,17 +1,47 @@
 const StylesPage = () => (
   <main className="m-4">
-    <section>
-      <h1>Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-      <h4>Heading 4</h4>
-      <h5>Heading 5</h5>
-      <h6>Heading 6</h6>
-    </section>
-    <section>
-      <p>I really like using Markdown.</p>
-      <p>I think I'll use it to format all of my documents from now on.</p>
-    </section>
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+    <h5>Heading 5</h5>
+    <h6>Heading 6</h6>
+
+    <p>I really like using Markdown.</p>
+    <p>I think I'll use it to format all of my documents from now on.</p>
+
+    <p>
+      This is the first line.
+      <br />
+      And this is the second line.
+    </p>
+
+    <p>
+      I just love <strong>bold text</strong>.
+    </p>
+    <p>
+      Love<strong>is</strong>bold
+    </p>
+    <p>
+      Italicized text is the <em>cat's meow</em>.
+    </p>
+    <p>
+      A<em>cat</em>meow.
+    </p>
+    <p>
+      This text is{" "}
+      <strong>
+        <em>really important</em>
+      </strong>
+      .
+    </p>
+    <p>
+      This is really
+      <strong>
+        <em>very</em>
+      </strong>
+      important text.
+    </p>
   </main>
 );
 
