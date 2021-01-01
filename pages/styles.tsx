f4a403fd82ac926979f5e7cfe893d9ca610eb43b
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-01 17:01:24 +0700

    Add p style

--- pages/styles.tsx
+++ pages/styles.tsx
@@ -1,13 +1,16 @@
 const StylesPage = () => (
-  <main>
+  <main className="m-4">
     <section>
-      <h1>Headings</h1>
       <h1>Heading 1</h1>
       <h2>Heading 2</h2>
       <h3>Heading 3</h3>
       <h4>Heading 4</h4>
       <h5>Heading 5</h5>
       <h6>Heading 6</h6>
+    </section>
+    <section>
+      <p>I really like using Markdown.</p>
+      <p>I think I'll use it to format all of my documents from now on.</p>
     </section>
   </main>
 );
