6f8d3478daed2319a1e54cc59ce9ababefeee23c
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-01 18:22:01 +0700

    Add image style

--- pages/styles.tsx
+++ pages/styles.tsx
@@ -200,6 +200,23 @@ const StylesPage = () => (
         , and that means comfort.
       </p>
     </blockquote>
+
+    <p>
+      <img
+        src="https://d33wubrfki0l68.cloudfront.net/eab45e25bb79970178fab7a2d10cba0209372a59/94d9e/assets/images/philly-magic-garden.jpg"
+        alt="Philadelphia's Magic Gardens. This place was so cool!"
+        title="Philadelphia's Magic Gardens"
+      />
+    </p>
+    <p>
+      <a href="https://www.flickr.com/photos/beaurogers/31833779864/in/photolist-Qv3rFw-34mt9F-a9Cmfy-5Ha3Zi-9msKdv-o3hgjr-hWpUte-4WMsJ1-KUQ8N-deshUb-vssBD-6CQci6-8AFCiD-zsJWT-nNfsgB-dPDwZJ-bn9JGn-5HtSXY-6CUhAL-a4UTXB-ugPum-KUPSo-fBLNm-6CUmpy-4WMsc9-8a7D3T-83KJev-6CQ2bK-nNusHJ-a78rQH-nw3NvT-7aq2qf-8wwBso-3nNceh-ugSKP-4mh4kh-bbeeqH-a7biME-q3PtTf-brFpgb-cg38zw-bXMZc-nJPELD-f58Lmo-bXMYG-bz8AAi-bxNtNT-bXMYi-bXMY6-bXMYv">
+        <img
+          src="https://d33wubrfki0l68.cloudfront.net/70a143fdf134aacde3740662a2a47a2a1ee0d216/276c9/assets/images/shiprock.jpg"
+          alt="An old rock in the desert"
+          title="Shiprock, New Mexico by Beau Rogers"
+        />
+      </a>
+    </p>
   </main>
 );
 
