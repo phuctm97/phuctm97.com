8ad699a1c9df6cbd261dc74925992e6868973aa4
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-03 17:11:21 +0700

    Fix header

--- components/header.tsx
+++ components/header.tsx
@@ -3,14 +3,14 @@ import { SiGithub } from "react-icons/si";
 
 const Header = () => (
   <header className="sticky top-0 z-10 bg-white bg-opacity-60 backdrop-blur">
-    <div className="container-custom container-custom-sm mx-auto flex flex-row items-center transition-all">
+    <div className="container-custom py-6 mx-auto flex flex-row items-center transition-all sm:py-8 lg:py-10">
       <h1 className="font-bold text-black text-base sm:text-lg md:text-xl">
         <Link href="/">@phuctm97</Link>
       </h1>
       <nav className="ml-auto">
         <Link href="https://github.com/phuctm97/phuctm97.com">
           <a
-            className="block w-5 sm:w-6 md:w-7 transition-all"
+            className="block w-5 transition-all sm:w-6 md:w-7"
             aria-label="View on Github"
           >
             <SiGithub
