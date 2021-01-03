ddb7e4624bad1bb652dbd887cab34e64bbaf4b32
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-03 17:25:11 +0700

    Update header and base styles

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import { SiGithub } from "react-icons/si";
 
 const Header = () => (
   <header className="sticky top-0 z-10 bg-white bg-opacity-60 backdrop-blur">
-    <div className="container-custom py-6 mx-auto flex flex-row items-center transition-all sm:py-8 lg:py-10">
+    <div className="container-custom py-6 mx-auto flex flex-row items-center transition-all md:py-8 lg:py-10">
       <h1 className="font-bold text-black text-base sm:text-lg md:text-xl">
         <Link href="/">@phuctm97</Link>
       </h1>
--- components/mdx/base.tsx
+++ components/mdx/base.tsx
@@ -5,7 +5,7 @@ export const wrapper: React.FC = ({ children }) => (
   <>
     <Header />
     <main className="container-custom mx-auto">
-      <article className="prose mx-auto prose-sm md:prose lg:prose-lg xl:prose-xl 2xl:prose-2xl">
+      <article className="prose prose-sm pt-4 pb-10 mx-auto md:prose md:pt-6 md:pb-12 lg:prose-lg lg:pt-8 lg:pb-16 xl:prose-xl 2xl:prose-2xl">
         {children}
       </article>
     </main>
