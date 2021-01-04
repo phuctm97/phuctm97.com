035185d02eafc2ca0ecf3f4485f6dc3a6f327e56
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-04 14:49:26 +0700

    Fix tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["pages/**/*.tsx", "components/**/*.tsx"],
+  purge: ["pages/**/*.tsx", "components/**/*.tsx", "layouts/**/*.tsx"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
