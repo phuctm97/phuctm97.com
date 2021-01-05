3cb1d25cb1f2fb16f218cbb8f73d12cc137bab61
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-05 14:14:32 +0700

    Update header and homepage styles

--- layouts/header.tsx
+++ layouts/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Header = () => (
   <nav className="sticky top-0 z-10 p-4 max-w-2xl mx-auto my-4 bg-white bg-opacity-80 md:px-0">
-    <h1 className="font-medium">
+    <h1 className="font-semibold text-gray-900">
       <Link href="/">Home</Link>
     </h1>
   </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,23 +9,23 @@ type Props = {
 
 const IndexPage = ({ blog }: Props) => (
   <>
-    <h1 className="font-black text-xl sm:text-3xl md:text-5xl tracking-tight">
+    <h1 className="font-black text-2xl tracking-tight text-gray-900 sm:text-3xl md:text-5xl">
       Hey, I’m Minh-Phuc Tran
     </h1>
-    <h2 className="mt-4 text-sm text-gray-700 sm:text-base">
+    <h2 className="mt-4 text-base sm:text-lg">
       I'm a Software Engineer. Welcome to my digital garden{" "}
-      <Emoji label="waving hand">👋🏻</Emoji>
+      <Emoji label="Waving hand">👋🏻</Emoji>
     </h2>
-    <h3 className="mt-2 text-sm text-gray-700 sm:text-base">
+    <h3 className="mt-2 text-base sm:text-lg">
       On this site, I document my journey of learning, making products, and
       pursuing freedom. You may expect to see{" "}
-      <strong className="font-semibold">3 articles per week</strong>.
+      <strong>3 articles per week</strong>.
     </h3>
-    <section className="mt-8">
-      <h2 className="font-bold tracking-tight text-xl sm:text-2xl md:text-3xl">
+    <section className="mt-10">
+      <h2 className="font-bold tracking-tight text-2xl text-gray-900 sm:text-3xl">
         Blog
       </h2>
-      <h3 className="mt-2 text-sm text-gray-700 sm:text-base">
+      <h3 className="mt-2 text-base sm:text-lg">
         Personal documentary: 100% authentic, not always well-researched, better
         gradually.
       </h3>
@@ -33,11 +33,11 @@ const IndexPage = ({ blog }: Props) => (
         <article key={path} className="mt-8">
           <Link href={path}>
             <a>
-              <h4 className="font-medium text-lg leading-5 text-gray-900 sm:text-xl">
+              <h4 className="font-semibold text-lg leading-5 sm:text-xl">
                 {title}
               </h4>
               <div className="overflow-hidden">
-                <p className="mt-2 text-xs text-gray-600 line-clamp-2 sm:text-sm">
+                <p className="mt-2 text-sm line-clamp-2 text-gray-500 sm:text-base">
                   {description}
                 </p>
               </div>
