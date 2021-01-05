ebf034ac5f2767c559f7c3a05d3145f8921f33f9
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-05 16:45:33 +0700

    Update header's styles

--- layouts/header.tsx
+++ layouts/header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 const Header = () => (
-  <nav className="sticky top-0 z-10 p-4 max-w-2xl mx-auto my-4 bg-white bg-opacity-80 md:px-0">
+  <nav className="sticky top-0 z-10 p-4 max-w-2xl mx-auto my-4 bg-white bg-opacity-80 backdrop-blur md:px-0 lg:max-w-3xl">
     <h1 className="font-semibold text-gray-900">
       <Link href="/">Home</Link>
     </h1>
