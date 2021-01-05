5ed12952dc97953684f504ddddcda6b8a79cbde3
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-05 09:53:07 +0700

    Add basic MDX wrapper

---
+++ components/mdx.tsx
@@ -0,0 +1,7 @@
+const Wrapper: React.FC = ({ children }) => (
+  <article className="prose prose-sm pt-4 pb-10 mx-auto md:prose md:pt-6 md:pb-12 lg:prose-lg lg:pt-8 lg:pb-16 xl:prose-xl 2xl:prose-2xl">
+    {children}
+  </article>
+);
+
+export { Wrapper as wrapper };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { MDXProvider } from "@mdx-js/react";
+import * as MDXComponents from "components/mdx";
 
 import "~styles/app.css";
 
@@ -8,7 +10,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head>
-    <Component {...pageProps} />
+    <MDXProvider components={MDXComponents}>
+      <Component {...pageProps} />
+    </MDXProvider>
   </>
 );
 
