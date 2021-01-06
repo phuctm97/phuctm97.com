6623677e89c819b028d51d6996f1c85ab10c4049
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-06 13:44:30 +0700

    Add a11y emojis

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const frontmatter = require("remark-frontmatter");
 const frontmatterParser = require("remark-parse-frontmatter");
 const prism = require("@mapbox/rehype-prism");
+const a11yEmojis = require("rehype-accessible-emojis").rehypeAccessibleEmojis;
 const { postSchema, postParser, postExporter } = require("./lib/js/post");
 
 const withMDX = require("@next/mdx")({
@@ -11,7 +12,7 @@ const withMDX = require("@next/mdx")({
       postParser,
       postExporter,
     ],
-    rehypePlugins: [prism],
+    rehypePlugins: [prism, a11yEmojis],
   },
 });
 
