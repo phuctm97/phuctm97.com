842019ee7e25a564545eae199cbd696442817a3c
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-06 11:47:25 +0700

    Add default SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
+import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
 
 import Header from "~layouts/header";
@@ -9,6 +10,7 @@ import Footer from "~layouts/footer";
 import { MDXProvider } from "@mdx-js/react";
 import * as MDXComponents from "components/mdx";
 
+import packageJSON from "~package.json";
 import "~styles/app.css";
 import "~styles/prism.css";
 
@@ -17,6 +19,22 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head>
+    <DefaultSeo
+      {...packageJSON.site}
+      canonical={packageJSON.homepage}
+      description={packageJSON.description}
+      openGraph={{
+        ...packageJSON.site.openGraph,
+        url: packageJSON.homepage,
+        title: packageJSON.site.title,
+        description: packageJSON.description,
+        images: packageJSON.site.openGraph.images.map((img) => ({
+          ...img,
+          url: `${packageJSON.homepage}${img.url}`,
+          alt: packageJSON.site.title,
+        })),
+      }}
+    />
     <ThemeProvider attribute="class" enableSystem={false}>
       <MDXProvider components={MDXComponents}>
         <Header />
