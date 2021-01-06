60d954c4f3a0acd04fe8af3bc8777253d6aab480
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-06 16:25:12 +0700

    Add dark mode for h1

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,6 +50,9 @@ module.exports = {
         dark: {
           css: {
             color: theme("colors.gray.300"),
+            "h1:first-of-type": {
+              color: theme("colors.gray.100"),
+            },
             "h1, h2, h3, h4, h5, h6, a, thead, :not(pre) code": {
               color: theme("colors.gray.100"),
             },
