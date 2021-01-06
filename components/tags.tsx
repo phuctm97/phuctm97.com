503a8006d139acd7d9e96354dfb7a7c9324646f3
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-06 18:02:38 +0700

    Update tags color

--- components/tags.tsx
+++ components/tags.tsx
@@ -5,7 +5,10 @@ type Props = {
 const Tags = ({ tags }: Props) => (
   <div className="tags flex flex-row flex-wrap mt-2 sm:mt-3 mb-8 text-sm">
     {tags.map((tag) => (
-      <p key={tag} className="bg-gray-100 dark:bg-gray-950 px-2 py-1 rounded">
+      <p
+        key={tag}
+        className="font-medium bg-gray-100 text-gray-600 dark:bg-gray-950 dark:text-gray-400 px-2 py-1 rounded"
+      >
         {tag}
       </p>
     ))}
