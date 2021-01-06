c8ecdd2a2afb016680d656e57aafaafb092955ac
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-06 10:23:16 +0700

    Use Next link in prose

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 const Wrapper: React.FC = ({ children }) => (
   <article className="prose prose-sm sm:prose dark:prose-dark">
     {children}
   </article>
 );
 
-export { Wrapper as wrapper };
+const Anchor = ({ href, ...htmlAttrs }: React.HTMLProps<HTMLAnchorElement>) =>
+  href ? (
+    <Link href={href}>
+      <a {...htmlAttrs} />
+    </Link>
+  ) : (
+    <a {...htmlAttrs} />
+  );
+
+export { Wrapper as wrapper, Anchor as a };
