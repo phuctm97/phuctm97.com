6320dd8a5d37e37491240a31c75a1a09100567f0
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-07 08:56:37 +0700

    Instruct Lighthouse to start server

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,5 +1,9 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ["http://localhost:3000/"],
+      startServerCommand: "yarn start",
+    },
     upload: {
       target: "temporary-public-storage",
     },
