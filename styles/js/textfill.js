c8e0f1f0bbdbbedc29a1ede3e6cc7ed2ae9fed11
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-08 11:35:04 +0700

    🎨 Add Tailwind autofill styling (#81)

---
+++ styles/js/textfill.js
@@ -0,0 +1,21 @@
+const plugin = require("tailwindcss/plugin");
+const flattenColors = require("./flatten-colors");
+
+const textfill = plugin(
+  ({ addUtilities, variants, theme, e }) => {
+    const colors = flattenColors(theme("colors"));
+    const utils = Object.entries(colors).reduce(
+      (res, [key, value]) =>
+        Object.assign(res, {
+          [`.${e(`textfill-${key}`)}`]: {
+            "-webkit-text-fill-color": value,
+          },
+        }),
+      {}
+    );
+    addUtilities(utils, variants("textfill"));
+  },
+  { variants: { textfill: [] } }
+);
+
+module.exports = textfill;
