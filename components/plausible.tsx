557c49383254e76bb75e23444e267c2f75e8b7ff
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-09 13:34:32 +0700

    🔨 Add custom domain for Plausible script

---
+++ components/plausible.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import packageJSON from "~package.json";
+
+const homepage = new URL(packageJSON.homepage);
+
+const PlausibleScript = () => {
+  return (
+    <Head>
+      <script
+        async
+        defer
+        data-domain={homepage.host}
+        src={packageJSON.plausible.scriptURL}
+      />
+    </Head>
+  );
+};
+
+export default PlausibleScript;
---
+++ js/utils/env.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isProd = void 0;
+exports.isProd = process.env.NODE_ENV === "production";
