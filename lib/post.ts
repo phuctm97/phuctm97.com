48636b9ae8b1c482a20b0c74dad73f56e99b42b8
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-13 08:57:54 +0700

    Move post exporter

--- lib/post.ts
+++ lib/post.ts
@@ -1,29 +1,13 @@
 import path from "path";
 import glob from "glob";
-import { Plugin } from "unified";
 import vfile from "to-vfile";
 
-import isParent from "@/unist-is-parent";
 import { reader } from "@/mdx-with-frontmatter";
 import { HasPost, Post } from "@/next-blog/interfaces";
 import parser from "@/next-blog/unified-parser";
 import { PAGES_DIR } from "@/next-constants";
 
 const BLOG_DIR = path.join(PAGES_DIR, "blog");
-
-/**
- * A unified/remark plugin that exports `post` from a _parsed_ MDX blog post for dynamic rendering (if applicable).
- */
-export const postExporter: Plugin = () => (tree, file) => {
-  const { post } = file.data as Partial<HasPost>;
-  if (!post) return file.message("Not a post, skip.");
-
-  if (!isParent(tree)) return file.fail("Tree is empty.");
-  tree.children.push({
-    type: "export",
-    value: `export const post = ${JSON.stringify(post)};`,
-  });
-};
 
 /**
  * Reads a blog post from a file.
