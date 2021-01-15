71925b6eacb9ae4c74ef513858f3b2cc785c30ae
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-15 08:53:28 +0700

    Separate layout

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,58 +1,10 @@
-import { Children } from "react";
-import { NextSeo } from "next-seo";
 import Link from "next/link";
-import Info from "~/components/info";
-import Tags from "~/components/tags";
-import Subscribe from "~/components/subscribe";
-import { Post } from "@/next-blog/interfaces";
-import packageJSON from "~/package.json";
 
-const { homepage } = packageJSON;
-const me = {
-  ...packageJSON.author,
-  avatarURL: "/static/avatar.jpg",
-  twitter: packageJSON.site.twitter.handle.substr(1),
-};
-
-const Wrapper: React.FC<{ post: Post }> = ({ post, children }) => {
-  const { description, tags, path } = post;
-  const title = `${post.title} | ${me.name}`;
-  const url = `${homepage}${path}`;
-  const date = new Date(post.date);
-
-  const [h1, ...content] = Children.toArray(children);
-  return (
-    <>
-      <NextSeo
-        title={title}
-        description={description}
-        canonical={url}
-        openGraph={{
-          type: "article",
-          title,
-          url,
-          description,
-          article: {
-            publishedTime: date.toISOString(),
-            authors: [me.url],
-            tags: tags,
-          },
-          images: [{ ...post.cover, alt: title }],
-        }}
-      />
-      <article className="prose prose-sm mx-auto sm:prose md:prose-md dark:prose-dark">
-        {h1}
-        <Info
-          author={{ ...me, url: `https://twitter.com/${me.twitter}` }}
-          date={date}
-        />
-        <Tags tags={tags} />
-        {content}
-      </article>
-      <Subscribe className="mt-14 md:mt-16" />
-    </>
-  );
-};
+const Wrapper = ({ children }: React.PropsWithChildren<{}>) => (
+  <article className="prose prose-sm mx-auto sm:prose md:prose-md dark:prose-dark">
+    {children}
+  </article>
+);
 
 const Anchor = ({ href, ...htmlAttrs }: React.HTMLProps<HTMLAnchorElement>) =>
   href ? (
