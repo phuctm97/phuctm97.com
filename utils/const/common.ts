891a3311f42b8e6bab76ad7eabcbc4276a7f01c9
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-22 20:43:08 +0700

    ✏️ Move package.json to const

--- utils/const/common.ts
+++ utils/const/common.ts
@@ -1,3 +1,10 @@
+import { URL } from "url";
+import PKG_JSON from "~/package.json";
+
+export { PKG_JSON };
+
+export const HOMEPAGE = new URL(PKG_JSON.homepage);
+
 export const IS_PRODUCTION =
   process.env.NODE_ENV === "production" &&
   process.env.VERCEL_ENV === "production";
