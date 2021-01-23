b3ebaf158ac74472edae01d90824d9c142a7c09d
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-23 15:03:24 +0700

    Remove old impl

--- js/utils/unified/compile-nothing.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function compileNothing() {
-    this.Compiler = () => "";
-}
-exports.default = compileNothing;
--- packages/next-blog/constants.ts
+++
@@ -1,4 +0,0 @@
-import path from "path";
-import { PAGES_DIR } from "~/utils/const/server";
-
-export const BLOG_DIR = path.join(PAGES_DIR, "blog");
