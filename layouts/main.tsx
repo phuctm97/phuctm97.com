02eaa6e8c6377b5152116d533f42c2b043c5452e
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-23 11:22:33 +0700

    ✏️ Update types

--- layouts/main.tsx
+++ layouts/main.tsx
@@ -1,4 +1,4 @@
-const Main: React.FC = ({ children }) => (
+const Main = ({ children }: React.PropsWithChildren<{}>) => (
   <main className="px-4 max-w-xl mx-auto mt-4 mb-16 md:max-w-2xl md:mt-6 md:mb-20">
     {children}
   </main>
