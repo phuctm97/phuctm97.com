e11e4465dec14d84074ac6e906d7e2b61b33230e
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-24 17:04:39 +0700

    🧹 Cleanup chore

--- next-env.d.ts
+++ next-env.d.ts
@@ -6,19 +6,24 @@ declare namespace NodeJS {
     readonly VERCEL_ENV: "production" | "preview" | "development";
   }
 }
+
 declare module "to-vfile" {
   import { VFile, VFileContents, VFileOptions } from "vfile";
+
   interface ToVFile {
     (input?: VFileContents | VFileOptions): VFile;
     readSync(path: string, encoding?: string): VFile;
   }
+
   const vfile: ToVFile;
   export default vfile;
 }
+
 declare module "@mdx-js/react" {
   export const MDXProvider: React.ComponentType<{ components: object }>;
 }
+
 declare module "*.mdx" {
-  const MDXDocument: React.ComponentType<any>;
+  const MDXDocument: React.ComponentType<{ [key: string]: unknown }>;
   export default MDXDocument;
 }
