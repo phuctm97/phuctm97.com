4364b4b265d77405165b6c1ad3b946f5d0d3ca72
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-24 14:29:23 +0700

    ♻️ Refactor header

--- layouts/header.tsx
+++ layouts/header.tsx
@@ -1,16 +1,14 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { useTheme } from "next-themes";
 import { FiSun, FiMoon } from "react-icons/fi";
+import { useTheme } from "next-themes";
 
 const Header = () => {
   const [isMounted, setMounted] = useState(false);
   useEffect(() => setMounted(true), []);
 
   const { theme, setTheme } = useTheme();
-  const onToggleDark = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
-  };
+  const onClickToggleDark = () => setTheme(theme === "dark" ? "light" : "dark");
 
   return (
     <nav className="sticky top-0 z-10 p-4 max-w-2xl mx-auto my-4 flex flex-row backdrop-blur md:px-0 lg:max-w-3xl">
@@ -21,7 +19,7 @@ const Header = () => {
         <button
           className="ml-auto p-2 rounded bg-gray-50 dark:bg-gray-950"
           aria-label="Toggle Dark mode"
-          onClick={onToggleDark}
+          onClick={onClickToggleDark}
         >
           {theme === "dark" ? <FiSun /> : <FiMoon className="fill-current" />}
         </button>
