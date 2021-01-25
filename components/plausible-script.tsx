3d414b3fc8eb6f9820632780d2fce7a2ba52957d
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-25 09:03:26 +0700

    ✏️ Fix code style in PlausibleScript

--- components/plausible-script.tsx
+++ components/plausible-script.tsx
@@ -1,18 +1,16 @@
 import Head from "next/head";
 import { PKG } from "~/constants/share";
 
-const PlausibleScript = () => {
-  return (
-    <Head>
-      <script
-        key="plausible-script"
-        src={PKG.site.plausible.scriptURL}
-        async
-        defer
-        data-domain={new URL(PKG.homepage).host}
-      />
-    </Head>
-  );
-};
+const PlausibleScript = () => (
+  <Head>
+    <script
+      key="plausible-script"
+      src={PKG.site.plausible.scriptURL}
+      async
+      defer
+      data-domain={new URL(PKG.homepage).host}
+    />
+  </Head>
+);
 
 export default PlausibleScript;
