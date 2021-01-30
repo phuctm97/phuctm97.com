dc5b744466e9ee61a1e6b2bbf26fba46e3310d1e
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-30 09:17:09 +0700

    🔍 Make code-block responsive (#154)

--- components/code-block/no-title.tsx
+++ components/code-block/no-title.tsx
@@ -5,7 +5,7 @@ type Props = React.PropsWithChildren<React.ComponentProps<typeof Copy>>;
 const NoTitle = ({ children, ...copy }: Props) => (
   <div className="relative group">
     {children}
-    <div className="absolute top-4 right-2.5 font-semibold text-sm opacity-0 group-hover:opacity-100 transition duration-200 ease-in-out">
+    <div className="absolute top-2.5 sm:top-3.5 right-2 sm:right-2.5 font-semibold text-xs sm:text-sm opacity-0 group-hover:opacity-100 focus-within:opacity-100 transition duration-200 ease-in-out">
       <Copy {...copy} />
     </div>
   </div>
