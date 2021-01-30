60de4296dcb48162f4dab27a2aa01604042a817a
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-30 09:14:30 +0700

    Fix hover styles

--- components/code-block/with-title.tsx
+++ components/code-block/with-title.tsx
@@ -7,7 +7,7 @@ type Props = React.PropsWithChildren<
 >;
 
 const WithTitle = ({ title, children, ...copy }: Props) => (
-  <div className="code-title group">
+  <div className="code-title">
     <div className="bg-gray-200 flex flex-row items-center justify-between px-3.5 sm:px-4 py-2 sm:py-3 rounded-t font-mono font-semibold text-xs sm:text-sm dark:bg-gray-700">
       {title}
       <Copy {...copy} />
