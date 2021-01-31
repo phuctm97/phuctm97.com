fddb9eb47a40b4fc863f317b8d51f25b68dc28ae
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-01-31 11:39:45 +0700

    🔨 Add useCopyableRef, write article about it

--- components/code-block/index.tsx
+++ components/code-block/index.tsx
@@ -1,11 +1,7 @@
-import { useRef, useState } from "react";
 import WithTitle from "~/components/code-block/with-title";
 import NoTitle from "~/components/code-block/no-title";
-import copy from "copy-to-clipboard";
 import classNames from "classnames";
-import { SECONDS } from "~/constants/share";
-
-const delayCopied = 4 * SECONDS;
+import useCopyableRef from "~/hooks/useCopyableRef";
 
 interface Props extends React.HTMLProps<HTMLPreElement> {
   "data-title"?: string;
@@ -21,31 +17,24 @@ const CodeBlock = ({
   className,
   ...props
 }: Props) => {
-  const ref = useRef<HTMLPreElement>(null);
+  const { ref, isCopied, copy } = useCopyableRef<HTMLPreElement>();
 
-  const [isCopied, setCopied] = useState(false);
-  const onClickCopy = () => {
-    if (isCopied) return;
-
-    if (!ref.current) throw new Error("Ref is nil.");
-    copy(ref.current.textContent || "");
-
-    setCopied(true);
-    setTimeout(() => setCopied(false), delayCopied);
-  };
-  const copyProps = { isCopied, onClickCopy };
+  const content = (
+    <pre ref={ref} className={classNames("content", className)} {...props} />
+  );
 
   return title ? (
     <WithTitle
       title={title}
       icon={icon || getLangIcon(className)}
-      {...copyProps}
+      isCopied={isCopied}
+      onClickCopy={copy}
     >
-      <pre ref={ref} className={classNames("content", className)} {...props} />
+      {content}
     </WithTitle>
   ) : (
-    <NoTitle {...copyProps}>
-      <pre ref={ref} className={classNames("content", className)} {...props} />
+    <NoTitle isCopied={isCopied} onClickCopy={copy}>
+      {content}
     </NoTitle>
   );
 };
