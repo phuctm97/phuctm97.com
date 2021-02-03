c1a4d6fb1fbafac890b913eaac962e9f8028f74d
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-02-03 21:16:50 +0700

    ✏️ Fix configs

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,12 +1,21 @@
+// Next.js defaults.
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 
+// Extra environment variables.
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly VERCEL_ENV: "production" | "preview" | "development";
   }
 }
 
+// Additional React types.
+declare namespace React {
+  interface HTMLPropsWithoutChildren<T>
+    extends Omit<HTMLProps<T>, "children"> {}
+}
+
+// Missing MDX and Unified types.
 declare module "to-vfile" {
   import { VFile, VFileContents, VFileOptions } from "vfile";
 
@@ -18,11 +27,9 @@ declare module "to-vfile" {
   const vfile: ToVFile;
   export default vfile;
 }
-
 declare module "@mdx-js/react" {
   export const MDXProvider: React.ComponentType<{ components: object }>;
 }
-
 declare module "*.mdx" {
   const MDXDocument: React.ComponentType<{ [key: string]: unknown }>;
   export default MDXDocument;
